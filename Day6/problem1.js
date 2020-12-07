6b5d074a4c2306beaf2cd6642ddb3261ec3c81f0
Jason Roell <roelljr@gmail.com>
2020-12-07 18:17:40 -0500

    update read.me and add template folders future days

---
+++ Day6/problem1.js
@@ -0,0 +1,9 @@
+/*
+
+*/
+import fs from 'fs';
+const input = fs.readFileSync(process.cwd() + '/Day13/testInput.txt').toString();
+const lines = input.split('\n');
+
+const answer = null;
+console.log(answer); // 248
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # adventCodeChallenge
+
+This project contains the solutions to the Advent of Code Challenge problems: (https://adventofcode.com/2020)
