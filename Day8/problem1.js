4c96fd9fd54f89d3ae454bee7395054524b1117e
Jason Roell <roelljr@gmail.com>
2020-12-08 02:04:11 -0500

    adds solutions to day 8 problems

--- Day8/problem1.js
+++ Day8/problem1.js
@@ -2,8 +2,39 @@
 
 */
 import fs from 'fs';
-const input = fs.readFileSync(process.cwd() + '/Day8/testInput.txt').toString();
-const lines = input.split('\n');
+const input = fs.readFileSync(process.cwd() + '/Day8/input.txt').toString();
 
-const answer = null;
-console.log(answer); // 248
+const lines = input.split('\n');
+const mem = [];
+let acc = 0;
+
+for (let i = 0; i < lines.length; ) {
+  if (mem.includes(i)) {
+    break;
+  } else {
+    mem.push(i);
+  }
+  const { opt, number } = parseLine(i);
+  switch (opt) {
+    case 'acc':
+      acc += number;
+      i++;
+      break;
+    case 'jmp':
+      i += number;
+      break;
+    case 'nop':
+      i++;
+      break;
+  }
+}
+
+function parseLine(i) {
+  const line = lines[i];
+  const opt = line.split(' ')[0];
+  const number = Number(line.split(' ')[1]);
+  return { opt, number };
+}
+
+// acc is the answer here
+console.log(acc); // 2025
